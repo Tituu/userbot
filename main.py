77e3a8a115b53b84c139167c4b708af1e9dda32f
Tituu <asatkarsarvesh39+anime@gmail.com>
2024-12-18 06:52:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ if __name__ == "__main__":
     
     # Run Flask App
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="https://tituu.koyeb.app/", port=port)
